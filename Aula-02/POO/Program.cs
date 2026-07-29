cac8be578a3bad336934535202c40e4946332d3a
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-07-29 16:39:58 -0300

    Aula 02 - Classe e Objeto

--- Aula-02/POO/Program.cs
+++ Aula-02/POO/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // comentário de linha
+        /* 
+        comentário de bloco
+        várias linhas
+         */
+
+        // escreve uma linha de texto na tela
+        Console.WriteLine("Programação Orientada a Objetos\n");
+
+        Console.WriteLine("Abstração - 1° Pilar da POO");
+
+        /* criar uma classe em C# 
+           a classe deve ser declarada fora do bloco de programa 
+        */
+
+        // criar um objeto a partir de uma classe
+
+        Pet pet1 = new Pet();
+
+        pet1.nome = "Totó";
+        pet1.tipo = "Cachorro";
+        pet1.idade = 3;
+
+        pet1.MostrarNome();
+
+
+        Pet pet2 = new Pet();
+
+        pet2.nome = "Mingau";
+        pet2.tipo = "Gato";
+        pet2.idade = 8;
+
+        pet2.MostrarNome();
+    }
+     public class Pet // nome da classe sempre inicia com letras MAIUSCÚLAS
+    {
+        // declaração dos atributos 
+        public string nome;
+        public string tipo;
+        public int idade;
+
+        // declaração dos métodos
+        public void MostrarNome()
+        {
+            Console.WriteLine("Meu nome é " + this.nome);
+        }
+    }
+}
