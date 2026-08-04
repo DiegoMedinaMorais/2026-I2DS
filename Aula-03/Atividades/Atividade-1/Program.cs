8ea90e757b4455a266a0359b20ac5ae6cc434d07
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-08-04 16:27:52 -0300

    Atividade1

---
+++ Aula-03/Atividades/Atividade-1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite a temperatura em celsius: ");
+       int celsius = int.Parse(Console.ReadLine());
+
+       int fahrenheit = (celsius * 9 / 5) + 32;
+       Console.WriteLine(celsius + "°C são " + fahrenheit + "°F.");
+    }
+}
