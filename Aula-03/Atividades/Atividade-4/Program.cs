22c146cec529c5dc898444076fac4076b90e24ee
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-08-04 17:06:30 -0300

    Atividade4 em andamento

--- Aula-03/Atividades/Atividade-4/Program.cs
+++ Aula-03/Atividades/Atividade-4/Program.cs
@@ -3,11 +3,18 @@
     private static void Main(string[] args)
     {
 
-        Console.Write("Digite o valor: ")
+        Console.Write("Digite o valor: ");
         int valor = int.Parse(Console.ReadLine());
 
         int nota = 100; // 100, 50, 20, 10, 5, 2 , 1
         int valor2 = 0;
+        int qntd100 = 0;
+        int qntd50 = 0;
+        int qntd20 = 0;
+        int qntd10 = 0;
+        int qntd5 = 0;
+        int qntd2 = 0;
+        int qntd1 = 0;
         while (nota != valor)
         {
             switch (nota)
@@ -19,12 +26,20 @@
                         valor2 -= nota;
                         nota = 50;
                     }
+                    else
+                    {
+                        qntd100++;
+                    }
                 case 50:
                     valor2 += nota;
                     if (valor2 > valor)
                     {
                         valor2 -= nota;
                         nota = 20;
+                    }
+                    else
+                    {
+                        qntd50++;
                     }
                 case 20:
                     valor2 += nota;
@@ -33,12 +48,20 @@
                         valor2 -= nota;
                         nota = 10;
                     }
+                    else
+                    {
+                        qntd20++;
+                    }
                 case 10:
                     valor2 += nota;
                     if (valor2 > valor)
                     {
                         valor2 -= nota;
                         nota = 5;
+                    }
+                    else
+                    {
+                        qntd10++;
                     }
                 case 5:
                     valor2 += nota;
@@ -47,6 +70,10 @@
                         valor2 -= nota;
                         nota = 2;
                     }
+                    else
+                    {
+                        qntd5++;
+                    }
                 case 2:
                     valor2 += nota;
                     if (valor2 > valor)
@@ -54,9 +81,44 @@
                         valor2 -= nota;
                         nota = 1;
                     }
+                    else
+                    {
+                        qntd2++;
+                    }
                 case 1:
                     valor2 += nota;
+                        qntd1++;
+                
             }
         }
+        if (qntd100 != 0);
+        {
+            Console.WriteLine(qntd100 + " notas de 100");
+        }
+        if (qntd50 != 0);
+        {
+            Console.WriteLine(qntd50 + " notas de 50");
+        }
+        if (qntd20 != 0);
+        {
+            Console.WriteLine(qntd20 + " notas de 20");
+        }
+        if (qntd10 != 0);
+        {
+            Console.WriteLine(qntd10 + " notas de 10");
+        }
+        if (qntd5 != 0);
+        {
+            Console.WriteLine(qntd5 != 0 + " notas de 5");
+        }
+        if (qntd2 != 0);
+        {
+            Console.WriteLine(qntd2 + " notas de 2");
+        }
+        if (qntd1 != 0);
+        {
+            Console.WriteLine(qntd1 + " notas de 1");
+        }
+
     }
 }
--- Aula-03/Atividades/Atividade-4/obj/Debug/net10.0/Atividade-4.AssemblyInfo.cs
+++ Aula-03/Atividades/Atividade-4/obj/Debug/net10.0/Atividade-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf826664e8165971f0b7de7edb9d9daac2cfc9e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cb2db78a482e27d582e07c6c0e245a689761be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
