9956a70823fb45a814329ed42d547f46b343c089
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-08-05 09:37:56 -0300

    Atividade6 meio completa

--- Aula-03/Atividades/Atividade-6/Program.cs
+++ Aula-03/Atividades/Atividade-6/Program.cs
@@ -2,6 +2,21 @@
 {
     private static void Main(string[] args)
     {
+        Console.Write("Digite um número: ");
+
+        int numero = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        int vezes = 10;
 
     }
+    public static void Contar(int numero, int vezes)
+    {
+        for (int i = 1; i <= vezes; i++)
+        {
+            Console.WriteLine(numero + " x " + i + " = " + numero * i);
+        }
+
+    }
+
+
 }
